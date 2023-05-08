5000bf4eb5c91511971241cdaf6b8fdc8a2d9b45
mohit20kumar <mohitkumar20021220@gmail.com>
2023-05-08 08:34:55 +0530

    added go back btn

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -4,6 +4,7 @@ import abi from "@/constants/abi"
 import contractAddresses from "@/constants/contractAddresses"
 import { ethers } from "ethers"
 import { useNotification } from "@web3uikit/core"
+import Link from "next/link"
 // import { abi, contractAddresses } from "@/constants/index"
 
 const LotteryEntrance = () => {
@@ -121,12 +122,28 @@ const LotteryEntrance = () => {
                         <p class="max-w-lg text-3xl font-semibold leading-loose text-gray-900 dark:text-white">
                             Recent Winner : {recentWinner}
                         </p>
+                        <Link href="/">
+                            <button class="relative inline-flex items-center justify-center p-0.5 mt-3 mb-2 overflow-hidden text-lg font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+                                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                    GO Back
+                                </span>
+                            </button>
+                        </Link>
                     </div>
                 </div>
             ) : (
-                <p class="max-w-lg text-3xl font-semibold leading-loose text-gray-900 dark:text-white">
-                    No Address Found!
-                </p>
+                <>
+                    <p class="max-w-lg text-3xl font-semibold leading-loose text-gray-900 dark:text-white">
+                        No Address Found!
+                    </p>
+                    <Link href="/">
+                        <button class="relative inline-flex items-center justify-center p-0.5 mt-3 mb-2 overflow-hidden text-lg font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+                            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                GO Back
+                            </span>
+                        </button>
+                    </Link>
+                </>
             )}
         </div>
     )
